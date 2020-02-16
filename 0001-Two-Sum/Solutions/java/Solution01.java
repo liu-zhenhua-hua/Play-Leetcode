9b4c5020a93751ad972735009042a9b546b1a79a
Liu Zhenhua <join_github_repo@126.com>
2020-02-16 22:28:48 +0800

    Leetcode 01 Two Sum Solution 01

--- 0001-Two-Sum/Solutions/java/Solution01.java
+++ 0001-Two-Sum/Solutions/java/Solution01.java
@@ -2,12 +2,17 @@
 * @Author: Tony Liu
 * @Date:   2020-02-16 22:24:32
 * @Modified by:   Tony Liu
-* @Last Modified time: 2020-02-16 22:26:52
+* @Last Modified time: 2020-02-16 22:28:42
 */
 
+
+/**
+   Brute Force
+   Time Complexity: O(n^2)
+*/
 class Solution01{
 
 	public int[] twoSum(int[] nums, int target){
-		
+
 	}
 }
