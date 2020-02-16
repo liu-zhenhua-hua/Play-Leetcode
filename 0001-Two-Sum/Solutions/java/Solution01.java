8813cfc0f4eca50536fa275df912cb0d8af10d63
Liu Zhenhua <join_github_repo@126.com>
2020-02-16 22:34:17 +0800

    Leetcode 01 Two Sum Solution 01

--- 0001-Two-Sum/Solutions/java/Solution01.java
+++ 0001-Two-Sum/Solutions/java/Solution01.java
@@ -2,7 +2,7 @@
 * @Author: Tony Liu
 * @Date:   2020-02-16 22:24:32
 * @Modified by:   Tony Liu
-* @Last Modified time: 2020-02-16 22:28:42
+* @Last Modified time: 2020-02-16 22:34:04
 */
 
 
@@ -13,6 +13,14 @@
 class Solution01{
 
 	public int[] twoSum(int[] nums, int target){
+		for(int i=0; i<nums.length;i++){
+			for(int j=i+1;j<nums.length;j++){
+				if(nums[j] == target - nums[i]){
+					return new int[] {i,j};
+				}
+			}
 
+		}
+		throw new IllegalArgumentException("No Solution for two Sum !");
 	}
 }
