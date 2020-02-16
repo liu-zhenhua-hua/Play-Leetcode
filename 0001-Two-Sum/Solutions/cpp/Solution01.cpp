09da9b3d0327066caf3d8182ea09a886c5cd8d4e
Liu Zhenhua <join_github_repo@126.com>
2020-02-16 22:50:47 +0800

    Leetcode 01 Two Sum Solution 01

--- 0001-Two-Sum/Solutions/cpp/Solution01.cpp
+++ 0001-Two-Sum/Solutions/cpp/Solution01.cpp
@@ -2,14 +2,20 @@
 * @Author: Tony Liu
 * @Date:   2020-02-16 22:40:01
 * @Modified by:   Tony Liu
-* @Last Modified time: 2020-02-16 22:42:10
+* @Last Modified time: 2020-02-16 22:50:33
 */
 #include<vector>
 
 using namespace std;
 
-class Solution
+class Solution{
 public:
 	vector<int> twoSum(vector<int>& nums, int target){
-		
+		for(int i=0; i< nums.size(); i++){
+			for(int j= i+1;i<nums.size();j++)
+				if(nums[j] = target - nums[i])
+					return {i,j};		
+		}
+		return {};
 	}
+}
