2b900c2b8c6dedb6f05af03d946b19178e1f4a40
Liu Zhenhua <join_github_repo@126.com>
2020-02-17 23:39:24 +0800

    LeetCode 01 Two Sum Solution

--- 0001-Two-Sum/Solutions/java/Solution03.java
+++ 0001-Two-Sum/Solutions/java/Solution03.java
@@ -2,12 +2,23 @@
 * @Author: Tony Liu
 * @Date:   2020-02-17 23:33:15
 * @Modified by:   Tony Liu
-* @Last Modified time: 2020-02-17 23:34:32
+* @Last Modified time: 2020-02-17 23:39:11
 */
 import java.util.Map;
 import java.util.HashMap;
 
 
 class Solution03{
+	public int[] twoSum(int[] nums,int target){
+		Map<Integer,Integer> map = new HashMap<>();
 
+		for(int i=0;i<nums.length;i++){
+			int elementKey = target - nums[i];
+			if(map.containsKey(elementKey)){
+				return new int[] {map.get(elementKey),i};
+			}
+			map.put(nums[i],i);
+		}
+		throw new IllegalArgumentException("No Two Sum Solution");
+	}
 }
