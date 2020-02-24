8ea0b64be18ee6f4856a6e7bd8fe0cb29fddbae8
Liu Zhenhua <join_github_repo@126.com>
2020-02-24 20:23:35 +0800

    LeetCode 02 Add Two Numbers ListNode

---
+++ 0002-Add-Two-Numbers/Solutions/java/src/Solution.java
@@ -0,0 +1,21 @@
+/*
+* @Author: Tony Liu
+* @Date:   2020-02-24 20:13:59
+* @Modified by:   Tony Liu
+* @Last Modified time: 2020-02-24 20:23:26
+*/
+
+public class Solution{
+
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+		if(l1 == null && l2 == null)
+			throw new IllegalArgumentException("Two ListNode is null !");
+
+		ListNode fake = new ListNode(0);
+		ListNode current = fake;
+		int carray = 0;
+
+	}
+
+
+}
