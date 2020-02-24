f77f7467a11a3560613a107d9e486e91c32c1381
Liu Zhenhua <join_github_repo@126.com>
2020-02-24 19:57:54 +0800

    LeetCode 02 Add Two Numbers ListNode

---
+++ 0002-Add-Two-Numbers/Solutions/java/src/ListNode.java
@@ -0,0 +1,16 @@
+/*
+* @Author: Tony Liu
+* @Date:   2020-02-24 19:55:37
+* @Modified by:   Tony Liu
+* @Last Modified time: 2020-02-24 19:57:00
+*/
+
+public class ListNode{
+	int val;
+	ListNode next;
+
+	public ListNode(int x){
+		val = x;
+	}
+
+}
