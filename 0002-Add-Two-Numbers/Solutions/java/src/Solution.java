b013c55262e4c5642208ba0fd8849032aa25ca23
Liu Zhenhua <join_github_repo@126.com>
2020-02-29 21:54:09 +0800

    LeetCode 02 Add Two Numbers ListNode

--- 0002-Add-Two-Numbers/Solutions/java/src/Solution.java
+++ 0002-Add-Two-Numbers/Solutions/java/src/Solution.java
@@ -2,7 +2,7 @@
 * @Author: Tony Liu
 * @Date:   2020-02-24 20:13:59
 * @Modified by:   Tony Liu
-* @Last Modified time: 2020-02-28 22:29:57
+* @Last Modified time: 2020-02-29 21:54:00
 *
 * 1) digit = l1.val + l2.val + carry; 
 *    val   = digit%10;
