0e82089dcc6b7487f7f110fdc710edce2b3f4ec1
Liu Zhenhua <join_github_repo@126.com>
2020-03-29 18:58:57 +0800

    0003-Longest-SubString-Without-Repeating-Characters

--- 0003-Longest-SubString-Without-Repeating-Characters/Solutions/java/src/Solution.java
+++ 0003-Longest-SubString-Without-Repeating-Characters/Solutions/java/src/Solution.java
@@ -1,9 +1,17 @@
 class Solution{
 	public int lengthOfLongestSubstring(String s){
+		/*
 		int[] count = new int[128];
 		int i = 0;
 		int j = 0;
-		
+		*/
+
+		int n = s.length();
+		int ans = 0;
+		for(int i=0;i<n;i++)
+			for(int j=i+1;j<=n;j++)
+				if(allUnique(s,i,j)) ans = Math.max(ans,j-i);
+		return ans;
 
 	}
 
